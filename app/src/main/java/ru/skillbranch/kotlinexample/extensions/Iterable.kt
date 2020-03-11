0ed1ec7bc9aa33470a5499ae094c1e816db3f4ba
aChesnovitskiy <a.chesnovitskiy@gmail.com>
2020-03-11 04:39:08 +0300

    finish additional task dropLastUntil

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,2 +1,35 @@
 package ru.skillbranch.kotlinexample.extensions
 
+/* Kotlin idiomatic variant */
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> =
+    run {
+        toMutableList().apply {
+            listIterator(size).apply {
+                while (hasPrevious()) {
+                    val element = previous()
+                    if (predicate(element)) {
+                        remove()
+                        break
+                    } else {
+                        remove()
+                    }
+                }
+            }
+        }.toList()
+    }
+
+/* Usual variant */
+//fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+//    val mutableList = this.toMutableList()
+//    val iterator = mutableList.listIterator(mutableList.size)
+//    while (iterator.hasPrevious()) {
+//        val element = iterator.previous()
+//        if (predicate(element)) {
+//            iterator.remove()
+//            break
+//        } else {
+//            iterator.remove()
+//        }
+//    }
+//    return mutableList.toList()
+//}
